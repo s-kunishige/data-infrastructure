d23aec20ba955ba5ce88ad47a7099004df3ab881
s-kunishige <s_kunishige@keywalker.jp>
2022-12-01 00:53:02 +0000

    フォルダ構成を整理

---
+++ embulk/plugins-install.sh
@@ -0,0 +1,6 @@
+embulk gem install representable -v 3.0.4
+embulk gem install public_suffix -v 4.0.7
+embulk gem install jwt:2.3.0
+embulk gem install embulk-input-bigquery
+embulk gem install embulk-filter-expand_json
+embulk gem install embulk-output-postgresql
